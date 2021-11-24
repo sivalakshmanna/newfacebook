223f1e38c65f036ef8dcde14be69669a212ac658
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-24 08:09:54 -0800

    upload deploye file

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -19,7 +19,7 @@ pipeline{
             steps{
                 println "copying artifacts"
                 //sh "ssh -i /tmp/sivalakshmanna07.pem ec2-user@${SERVER_IP}" 
-                sh "scp -i /tmp/sivalakshmanna07.pem hello-${BUILDNUMBER}.war ec2-user@${SERVER_IP}:/tmp/"
+                sh "scp -i /tmp/sivalakshmanna07.pem hello-${BUILD_NUMBER}.war ec2-user@${SERVER_IP}:/tmp/"
                 sh "ssh -i /tmp/sivalakshmanna07.pem ec2-user@${SERVER_IP} \"sudo cp /tmp/hello-${BUILD_NUMBER}.war /var/lib/tomcat/webapps\""
             }
         }
