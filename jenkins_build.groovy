c4c78e319afd1f90fe0696a3c3b943ef2d83a06a
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-24 10:27:50 -0800

    upload build file

--- jenkins_build.groovy
+++ jenkins_build.groovy
@@ -1,31 +1,32 @@
- pipeline{
-    agent any 
-    environment{
+pipeline{
+    agent any
+    environment {
         BRANCH = "${env.BRANCH_NAME}"
     }
     stages{
-        stage("clone the code"){
+        stage("chekout code"){
             steps{
-                println "clonig the code from git hub"
+                println "clone our code to our repository"
                 sh "ls -l"
                 sh "ls -lart ./*"
                 git branch: "${BRANCH_NAME}",
-                url:  'https://github.com/sivalakshmanna/boxfuse-sample-java-war-hello.git
+                url: 'https://github.com/KuruvaSomaSekhar/boxfuse-sample-java-war-hello.git'
                 
+
             }
         }
-        stage("build the code"){
+        stage("build code"){
             steps{
-                println "build the code"
+                println "mvn clean package"
                 sh "mvn clean package"
-                sh "ls -l"
+                sh "ls -l target/"
             }
         }
         stage("upload artifacts to s3"){
             steps{
-                println "upload artifacts to s3 bucket"
+                println "uploading artifacts to s3 bucket"
                 sh "echo $BUILD_NUMBER"
-                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://sivabandela/"
+               sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://sivabandela/${BRANCH}/${BUILD_NUMBER}/"
             }
         }
     }
