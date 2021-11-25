d02b4c52a09f4aca19315b76df99eab8ddef90a6
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-24 20:51:51 -0800

    upload deploye file

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -1,9 +1,9 @@
 pipeline{
     agent any 
     parameters{
-        string(name: 'BRANCH_NAME', defaultvalue: 'master', description: 'give branch name')
-        string(name: 'SERVER_IP', defaultvalue: '', description: 'give serverip address')
-        string(name: 'BUILD_NUMBER', defaultvalue: '', description: 'give build number')
+        string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'give branch name')
+        string(name: 'SERVER_IP', defaultValue: '', description: 'give serverip address')
+        string(name: 'BUILD_NUMBER', defaultValue: '', description: 'give build number')
     }
     stages{
         stage("download artifacts"){
@@ -18,7 +18,7 @@ pipeline{
         stage("copy artifacts"){
             steps{
                 println "copying artifacts"
-                sh "ssh -i /tmp/sivalakshmanna07.pem ec2-user@${SERVER_IP}" 
+                //sh "ssh -i /tmp/sivalakshmanna07.pem ec2-user@${SERVER_IP}" 
                 sh "scp -i /tmp/sivalakshmanna07.pem hello-${BUILDNUMBER}.war ec2-user@${SERVER_IP}:/tmp/"
                 sh "ssh -i /tmp/sivalakshmanna07.pem ec2-user@${SERVER_IP} \"sudo cp /tmp/hello-${BUILD_NUMBER}.war /var/lib/tomcat/webapps\""
             }
